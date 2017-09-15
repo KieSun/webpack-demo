0120c9df98d6e1b506b653f8ee084ba11efc5bc7
俞诚恺 <zx597813039@gmail.com>
2017-09-15 15:44:58 +0800

    处理 CSS

--- app/addImage.js
+++ app/addImage.js
@@ -1,7 +1,9 @@
+import '../styles/addImage.css'
+
 let smallImg = document.createElement('img')
 smallImg.src = require('../images/small.jpeg')
 document.body.appendChild(smallImg)
 
-let bigImg = document.createElement('img')
-bigImg.src = require('../images/big.jpeg')
-document.body.appendChild(bigImg)
+// let bigImg = document.createElement('img')
+// bigImg.src = require('../images/big.jpeg')
+// document.body.appendChild(bigImg)
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,44 @@
 const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
 module.exports = {
-    entry:  './app/index.js', // 入口文件
+    entry: './app/index.js', // 入口文件
     output: {
-      path: path.resolve(__dirname, 'build'), // 必须使用绝对地址，输出文件夹
-      filename: "bundle.js", // 打包后输出文件的文件名
-      publicPath: 'build/'
+        path: path.resolve(__dirname, 'build'), // 必须使用绝对地址，输出文件夹
+        filename: "bundle.js", // 打包后输出文件的文件名
+        publicPath: 'build/' // 打包后的文件夹
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.js$/,
                 use: 'babel-loader',
                 exclude: /node_modules/
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                use: [
-                  {
+                use: [{
                     loader: 'url-loader',
                     options: {
-                      limit: 10000,
-                      name: 'images/[name].[hash].[ext]'
-                   }
-                  }
-                ]
-            }
+                        limit: 10000,
+                        name: 'images/[name].[hash].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: { 
+                            modules: true
+                        }
+                    }]
+                })
+            },
         ]
-    }
-  }
+    },
+    plugins: [
+      new ExtractTextPlugin("css/[name].[hash].css")
+    ]
+}
