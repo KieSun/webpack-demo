f2756d6f96515bd673001ec94f52cdc8d9a88717
yuchengkai <zx597813039@gmail.com>
2017-09-16 21:56:09 +0800

    代码分离第一步

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,15 +1,28 @@
 var webpack = require('webpack');
 var path = require('path');
 
+const VENOR = ["faker",
+  "lodash",
+  "react",
+  "react-dom",
+  "react-input-range",
+  "react-redux",
+  "redux",
+  "redux-form",
+  "redux-thunk"
+]
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENOR
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
-    rules:[
-      {
+    rules: [{
         test: /\.js$/,
         use: 'babel-loader'
       },
